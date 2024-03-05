5a4694bcf531dbfc633af3c9b2e38143967b62cc
Massimiliano Picone <87522343+Pmax8@users.noreply.github.com>
2024-03-05 17:54:41 +0100

    Update ruby_intro.rb

--- ruby-intro/book-lab/lib/ruby_intro.rb
+++ ruby-intro/book-lab/lib/ruby_intro.rb
@@ -16,12 +16,11 @@ def sum arr
     return sum
   end
 end
-
+=begin
 def max_2_sum arr
   #Define a method `max_2_sum(array)` which takes an array of integers as an argument and returns the sum of its two largest elements. For an empty array it should return zero. For an array with just one element, it should return that element (Consider if the two largest elements are the same value as well). 
   #Run associated tests via:  `$ rspec -e '#max_2_sum' spec/part1_spec.rb`
   # YOUR CODE HERE
-=begin
   if arr truthy
     return 0
   else 
@@ -39,7 +38,7 @@ def max_2_sum arr
     end
   end
 end
-=end 
+
 def sum_to_n? arr, n
   #Define a method `sum_to_n?(array, n)` that takes an array of integers and an additional integer, n, as arguments and returns true if any two elements in the array of integers sum to n. `sum_to_n?([], n)` should return false for any value of n, by definition. 
   #Run associated tests via:  `$ rspec -e '#sum_to_n' spec/part1_spec.rb` 
@@ -80,3 +79,4 @@ class BookInStock
   #Include a method `price_as_string` that returns the price of the book formatted with a leading dollar sign and two decimal places, that is, a price of 20 should format as `$20.00` and a price of 33.8 should format as `$33.80`. Check out formatted string methods in Ruby. Run associated tests via:  `$ rspec -e '#price_as_string' spec/part3_spec.rb`
 # YOUR CODE HERE
 end
+=end
