b73cfc2f22d93f3d0562ab76c1fa4687977788e2
Massimiliano Picone <87522343+Pmax8@users.noreply.github.com>
2024-03-05 17:23:28 +0100

    Update ruby_intro.rb

--- ruby-intro/book-lab/lib/ruby_intro.rb
+++ ruby-intro/book-lab/lib/ruby_intro.rb
@@ -8,6 +8,7 @@ def sum arr
   # YOUR CODE HERE
   sum = 0
   if arr truthy
+    return 0
   else 
     arr.each do |n|
     sum += n
@@ -17,12 +18,24 @@ def max_2_sum arr
   #Define a method `max_2_sum(array)` which takes an array of integers as an argument and returns the sum of its two largest elements. For an empty array it should return zero. For an array with just one element, it should return that element (Consider if the two largest elements are the same value as well). 
   #Run associated tests via:  `$ rspec -e '#max_2_sum' spec/part1_spec.rb`
   # YOUR CODE HERE
+  if arr truthy
+    return 0
+  else 
+    max1 = 0
+    max2 = 0
+    arr.each do |n|
+    if n>max1 
+      max1=n
+    elsif n<max1 && n>max2
+      max2=n
+    return max1+max2
 end
 
 def sum_to_n? arr, n
   #Define a method `sum_to_n?(array, n)` that takes an array of integers and an additional integer, n, as arguments and returns true if any two elements in the array of integers sum to n. `sum_to_n?([], n)` should return false for any value of n, by definition. 
   #Run associated tests via:  `$ rspec -e '#sum_to_n' spec/part1_spec.rb` 
   # YOUR CODE HERE
+  
 end
 
 # Part 2
