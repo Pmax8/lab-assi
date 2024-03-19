83c8c0db9aefcdfafb349f9159b8d429184bd341
Massimiliano Picone <87522343+Pmax8@users.noreply.github.com>
2024-03-19 18:11:36 +0100

    Update movies_controller.rb

--- rails-sort-index/app/controllers/movies_controller.rb
+++ rails-sort-index/app/controllers/movies_controller.rb
@@ -4,6 +4,12 @@ class MoviesController < ApplicationController
   # GET /movies or /movies.json
   def index
     @movies = Movie.all
+    if params[:sort] == "Title"
+      @movies = Movie.order(:title)
+    end
+    if params[:sort] == "Release Date"
+      @movies = Movie.order(:release_date)
+    end
   end
 
   # GET /movies/1 or /movies/1.json
@@ -68,3 +74,4 @@ class MoviesController < ApplicationController
       params.require(:movie).permit(:title, :rating, :description, :release_date)
     end
 end
+
